00ee0881cdc82c295e2367b920c0afd9f18d8ee9
addaddudda <hwon3330@naver.com>
2025-09-07 22:29:35 +0900

    일단 말은 하게 만듬

--- src/main/java/commands/villager.java
+++ src/main/java/commands/villager.java
@@ -1,4 +1,43 @@
 package commands;
 
-public class vilager {
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+import java.util.Objects;
+
+public class villager extends JavaPlugin implements CommandExecutor{
+
+    public static class villager_create implements CommandExecutor{
+        @Override
+        public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+            if (!(sender instanceof Player)) return false;
+            Player p = (Player)sender;
+            if(args.length > 2) {
+                p.sendMessage("/villager_create");
+            }else if(args.length == 1){
+                Location loc = p.getLocation();
+                Villager villager = (Villager)loc.getWorld().spawnEntity(loc, EntityType.VILLAGER);
+                villager.setCustomName(args[0]);
+                villager.setCustomNameVisible(true);
+                villager.setAI(false);
+            }else{
+                p.sendMessage("명령어 이상");
+            }
+            return false;
+        }
+    }
+
 }
--- src/main/java/practice/staetplugin/Staetplugin.java
+++ src/main/java/practice/staetplugin/Staetplugin.java
@@ -1,6 +1,8 @@
 package practice.staetplugin;
 
 import commands.statecommand;
+import commands.villager;
+import net.md_5.bungee.api.chat.ClickEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -10,16 +12,21 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.C;
 import org.jetbrains.annotations.NotNull;
-
+import net.md_5.bungee.api.chat.TextComponent;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,6 +37,7 @@ public final class Staetplugin extends JavaPlugin implements Listener, CommandEx
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("stat").setExecutor(new statecommand.state());
+        getCommand("villager_create").setExecutor(new villager.villager_create());
         getCommand("reload_stat").setExecutor(this);
     }
 
@@ -84,8 +92,32 @@ public final class Staetplugin extends JavaPlugin implements Listener, CommandEx
         stat_point+=3;
         p.sendMessage("§e스텟 포인트가 3만큼 올랐습니다!");
         p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
-    }
+    }/*
+    @EventHandler
+    public void damaged_villager(EntityDamageEvent event){
+        if(event.getEntity() instanceof Villager){
+            Villager villager = (Villager) event.getEntity();
+            if("전직".equals(villager.getCustomName())){
+                event.setCancelled(true);
+            }
+        }
+    }*/
+    @EventHandler
+    public void interacted_villager(PlayerInteractAtEntityEvent event){
+        Player p = event.getPlayer();
+        if (event.getRightClicked().getCustomName().equals("전직")) {
+            TextComponent tanker = new TextComponent("[탱커] 방어력 스텟이 다른 직업들보다 더 높게 올라간다.");
+            tanker.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/choosedjob tanker"));
+            TextComponent assassin = new TextComponent("[암살자] 속도 스텟이 다른 직업들보다 더 높게 올라간다.");
+            assassin.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/choosedjob tanker"));
+            TextComponent attacker = new TextComponent("[딜러] 공격력 스텟이 다른 직업들보다 더 높게 올라간다.");
+            attacker.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/choosedjob tanker"));
+            p.spigot().sendMessage(tanker);
+            p.spigot().sendMessage(assassin);
+            p.spigot().sendMessage(attacker);
 
+        }
+    }
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player p = (Player)commandSender;
